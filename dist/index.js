a4320e461c1bff98f1a2d3e1bd50a740f41e8531
daldalhada <ssen199545@gmail.com>
2021-01-18 18:58:07 +0900

    typescript init

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nick = "daldahada", age = 24, gender = "male";
+const printing = (name, age, gender) => {
+    console.log(`nick: ${nick}, age: ${age}, gender: ${gender}`);
+};
+printing(nick, age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const nick = "daldahada",
+    age = 24,
+    gender = "male";
+
+const printing = (name , age, gender?) => {
+    console.log(`nick: ${nick}, age: ${age}, gender: ${gender}`);
+};
+
+printing(nick, age);
+
+export {};
+
